0d3a846f884298aa6f3322f577fdd756a0ad1cae
Xavier Barriga <xbarriga@technisys.com>
2023-07-20 11:39:50 -0500

    Primer commit

---
+++ Desktop/React/06-mis-custom-hooks/useTodos.js
@@ -0,0 +1,54 @@
+import { useEffect, useReducer } from 'react';
+import { todoReducer } from '../08-useReducer/todoReducer';
+
+const initialState = [];
+        
+const init = () => {
+    return JSON.parse( localStorage.getItem('todos')) || [];
+}        
+ 
+ export const useTodos = () => {  
+               
+    const [ todos, dispatch ] = useReducer( todoReducer, [], init );
+
+    useEffect(() => {
+    //   console.log(todos);
+        localStorage.setItem('todos', JSON.stringify( todos ) );
+    }, [todos])
+
+    const handleNewTodo = ( todo ) => {
+        // console.log({ todo });
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        }
+
+        dispatch( action );
+    }
+
+    const handleDeleteTodo = ( id ) => {        
+        // console.log({ id })
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id
+        });
+    }
+
+    const handleToggleTodo = ( id ) => {        
+        // console.log({ id })
+        dispatch({
+            type: '[TODO] Toggle Todo',
+            payload: id
+        });
+    }    
+
+   return {
+        todos,
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter(todo=> !todo.done).length,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo,
+   }
+ }
+ 
